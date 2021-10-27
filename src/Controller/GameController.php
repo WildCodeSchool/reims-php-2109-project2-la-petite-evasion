a01f2b95bbdfb91a075a4704d7e4e5b457eebb2f
Aloe Vera <rimie.nkou.20@neoma-bs.com>
2021-10-27 10:25:13 +0200

    GameController: Add method index

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -2,6 +2,18 @@
 
 namespace App\Controller;
 
+use App\Model\LevelManager;
+
 class GameController extends AbstractController
 {
+    /**
+     * Show first level
+     */
+    public function index(): string
+    {
+        $levelManager = new LevelManager();
+        $level = $levelManager->selectOneById(1);
+
+        return $this->twig->render('Game/index.html.twig', ['game' => $level]);
+    }
 }
