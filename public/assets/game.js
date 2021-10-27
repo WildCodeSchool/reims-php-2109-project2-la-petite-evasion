d31a1344d5b7d2431efc13e4aed6f96ad10febbc
Mario Rocha <mrocha51248@gmail.com>
2021-10-27 12:09:58 +0200

    game.js: define game constants and player default attributes

--- public/assets/game.js
+++ public/assets/game.js
@@ -3,3 +3,13 @@ var gameDiv = document.querySelector(".game");
 const levelContent = gameDiv.dataset.levelContent.split(',').map(x => x.split(''));
 const levelHeight = levelContent.length;
 const levelWidth = levelContent[0].length;
+
+const CELL_WALL = "1";
+const CELL_FLOOR = "0";
+
+const VIEWPOINT_RADIUS = 2;
+
+let player = {
+    x: 0,
+    y: 0,
+};
