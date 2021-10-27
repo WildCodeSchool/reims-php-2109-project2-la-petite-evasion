ecb68feb359a23ff8c1c546c99d7c395f89f3baf
Mario Rocha <mrocha51248@gmail.com>
2021-10-27 12:11:23 +0200

    game.js: draw tiles around player

--- public/assets/game.js
+++ public/assets/game.js
@@ -13,3 +13,27 @@ let player = {
     x: 0,
     y: 0,
 };
+
+for (y = player.y - VIEWPOINT_RADIUS; y <= player.y + VIEWPOINT_RADIUS; ++y) {
+    let row = Array(levelWidth).fill(CELL_WALL);
+    if (levelContent[y] !== undefined) {
+        row = levelContent[y];
+    }
+
+    for (x = player.x - VIEWPOINT_RADIUS; x <= player.x + VIEWPOINT_RADIUS; ++x) {
+        let cellData = CELL_WALL;
+        if (row[x] !== undefined) {
+            cellData = row[x];
+        }
+
+        var tile = document.createElement("div");
+        if (x === player.x && y === player.y) {
+            tile.classList.add("player");
+        } else if (cellData === CELL_FLOOR) {
+            tile.classList.add("floor");
+        } else if (cellData === CELL_WALL) {
+            tile.classList.add("wall");
+        }
+        gameDiv.appendChild(tile);
+    }
+}
