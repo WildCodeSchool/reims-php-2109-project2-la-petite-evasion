1212e6cabe1454b2e389250350df260e2994c6e9
Aloe Vera <rimie.nkou.20@neoma-bs.com>
2021-10-28 11:43:23 +0200

    WinController: delete spaces after the curly braces

--- src/Controller/WinController.php
+++ src/Controller/WinController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 class WinController extends AbstractController
-{    
+{
     public function index(): string
     {
         return $this->twig->render('Game/win-condition.html.twig');
