f026c37fc4af96524f2134686e0aba3254eb7202
Aloe Vera <rimie.nkou.20@neoma-bs.com>
2021-10-28 11:17:20 +0200

    WinController: Edit curly braces

--- src/Controller/WinController.php
+++ src/Controller/WinController.php
@@ -2,8 +2,8 @@
 
 namespace App\Controller;
 
-class WinController extends AbstractController {
-    
+class WinController extends AbstractController
+{    
     public function index(): string
     {
         return $this->twig->render('Game/win-condition.html.twig');
