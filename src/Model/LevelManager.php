c64865904e34b8c89b6943e2d1951586f83af163
Sayn <marvincrepin@gmail.com>
2021-11-03 11:42:27 +0100

    Define const in LevelManager

--- src/Model/LevelManager.php
+++ src/Model/LevelManager.php
@@ -5,4 +5,6 @@ namespace App\Model;
 class LevelManager extends AbstractManager
 {
     public const TABLE = 'level';
+    public const CELL_WALL = 'wall';
+    public const CELL_FLOOR = 'floor';
 }
