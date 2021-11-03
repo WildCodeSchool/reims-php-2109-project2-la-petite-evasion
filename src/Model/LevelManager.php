3b4a6cb34dfe6d73bd795026e79ea23f8dbd1e5c
Sayn <marvincrepin@gmail.com>
2021-11-03 11:44:13 +0100

    Level Manager : Implement parseContent method

--- src/Model/LevelManager.php
+++ src/Model/LevelManager.php
@@ -7,4 +7,21 @@ class LevelManager extends AbstractManager
     public const TABLE = 'level';
     public const CELL_WALL = 'wall';
     public const CELL_FLOOR = 'floor';
+
+    public static function parseContent(string $content): array 
+    {
+        $cells = [];
+        foreach (explode(',', $content) as $row) {
+            $cellRow = [];
+            foreach (str_split($row) as $cell) {
+                if ($cell === '1') {
+                    $cellRow[] = self::CELL_WALL;
+                } else {
+                    $cellRow[] = self::CELL_FLOOR;
+                }
+            }
+            $cells[] = $cellRow;
+        }
+        return $cells;
+    }
 }
