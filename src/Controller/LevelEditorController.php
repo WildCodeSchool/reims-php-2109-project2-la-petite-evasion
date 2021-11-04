8fdd0ce34903d5bc5552da4641d48de4846fb237
Sayn <marvincrepin@gmail.com>
2021-11-04 11:22:26 +0100

    LevelEditor : Added inputs checks

--- src/Controller/LevelEditorController.php
+++ src/Controller/LevelEditorController.php
@@ -14,42 +14,16 @@ class LevelEditorController extends AbstractController
         $errors = [];
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            // clean $_POST data
             $cells = [];
-
-            if (empty($_POST['name']) || $_POST['name'] > 30) {
-                $errors[] = 'Le nom est invalide';
-            }
-            if (!$errors) {
-                $name = $_POST['name'];
-                $description = $_POST['description'] ?? '';
-                $level['name'] = $name;
-                $level['description'] = $description;
-                array_map('trim', $level);
-            }
-
-            foreach ($_POST as $entry => $value) {
-                $capture = [];
-                $matched = preg_match("/^cell-(?<x>[0-9]+)-(?<y>[0-9]+)$/", $entry, $capture);
-                if ($matched === 1) {
-                    if (array_key_exists('x', $capture) && array_key_exists('y', $capture)) {
-                        $posX = intval($capture['x']);
-                        $posY = intval($capture['y']);
-                        if (!array_key_exists($posY, $cells)) {
-                            $cells[$posY] = [];
-                        }
-                        $cells[$posY][$posX] = $value;
-                    }
-                }
-            }
-            foreach ($cells as $row) {
+            $this->parsePost($level, $cells, $errors);
+            array_walk($cells, function ($row) {
                 ksort($row);
-                if (count($row) - 1 === array_key_last($row)) {
+                if (count($row) - 1 !== array_key_last($row)) {
                     $errors[] = 'Cellules invalides';
                 }
-            }
+            });
             ksort($cells);
-            if (count($cells) - 1 === array_key_last($cells)) {
+            if (count($cells) - 1 !== array_key_last($cells)) {
                 $errors[] = 'Lignes invalides';
             }
             if (!$errors) {
@@ -58,5 +32,30 @@ class LevelEditorController extends AbstractController
             }
         }
         return $this->twig->render('Editor/edit.html.twig', ['level' => $level, 'grid' => $grid]);
+    }
+
+    private function parsePost(array &$level, array &$cells, array &$errors): void
+    {
+        if (empty($_POST['name']) || strlen($_POST['name']) > 30) {
+            $errors[] = 'Le nom est invalide';
+        }
+        if (!$errors) {
+            $name = $_POST['name'];
+            $description = $_POST['description'] ?? '';
+            $level['name'] = $name;
+            $level['description'] = $description;
+            array_map('trim', $level);
+        }
+
+        foreach ($_POST as $entry => $value) {
+            $capture = [];
+            $matched = preg_match("/^cell-(?<x>[0-9]+)-(?<y>[0-9]+)$/", $entry, $capture);
+            if ($matched === 1 && array_key_exists('x', $capture) && array_key_exists('y', $capture)) {
+                $posX = intval($capture['x']);
+                $posY = intval($capture['y']);
+                $cells[$posY] = $cells[$posY] ?? [];
+                $cells[$posY][$posX] = $value;
+            }
+        }
     }
 }
