5a867146a7bfd9a95463811a7b01572ad726305f
Sayn <marvincrepin@gmail.com>
2021-11-04 11:17:43 +0100

    LevelEditor : Added inputs checks

--- src/Controller/LevelEditorController.php
+++ src/Controller/LevelEditorController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Model\LevelManager;
 
-
 class LevelEditorController extends AbstractController
 {
     public function edit(int $id)
@@ -12,36 +11,48 @@ class LevelEditorController extends AbstractController
         $levelManager = new LevelManager();
         $level = $levelManager->selectOneById($id);
         $grid = LevelManager::parseContent($level['content']);
+        $errors = [];
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             // clean $_POST data
             $cells = [];
 
-            if (!empty($_POST['name'])) {
+            if (empty($_POST['name']) || $_POST['name'] > 30) {
+                $errors[] = 'Le nom est invalide';
+            }
+            if (!$errors) {
                 $name = $_POST['name'];
-                $description = $_POST['description'];
+                $description = $_POST['description'] ?? '';
                 $level['name'] = $name;
                 $level['description'] = $description;
+                array_map('trim', $level);
+            }
 
-                foreach ($_POST as $entry => $value) {
-                    $capture = [];
-                    $matched = preg_match("/^cell-(?<x>[0-9]+)-(?<y>[0-9]+)$/", $entry, $capture);
-                    if ($matched === 1) {
-                        if (array_key_exists('x', $capture) && array_key_exists('y', $capture)) {
-                            $x = intval($capture['x']);
-                            $y = intval($capture['y']);
-                            if (!array_key_exists($y, $cells)) {
-                                $cells[$y] = [];
-                            }
-                            $cells[$y][$x] = $value;
+            foreach ($_POST as $entry => $value) {
+                $capture = [];
+                $matched = preg_match("/^cell-(?<x>[0-9]+)-(?<y>[0-9]+)$/", $entry, $capture);
+                if ($matched === 1) {
+                    if (array_key_exists('x', $capture) && array_key_exists('y', $capture)) {
+                        $posX = intval($capture['x']);
+                        $posY = intval($capture['y']);
+                        if (!array_key_exists($posY, $cells)) {
+                            $cells[$posY] = [];
                         }
+                        $cells[$posY][$posX] = $value;
                     }
                 }
-                foreach ($cells as $row) {
-                    ksort($row);
+            }
+            foreach ($cells as $row) {
+                ksort($row);
+                if (count($row) - 1 === array_key_last($row)) {
+                    $errors[] = 'Cellules invalides';
                 }
-                ksort($cells);
-                var_dump($cells);
+            }
+            ksort($cells);
+            if (count($cells) - 1 === array_key_last($cells)) {
+                $errors[] = 'Lignes invalides';
+            }
+            if (!$errors) {
                 $levelManager = new LevelManager();
                 $levelManager->update($level, $cells);
             }
