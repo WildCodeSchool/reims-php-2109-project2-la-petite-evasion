520cd47a6290468d1aee52bebea829b5fa878769
So-Gau <sophie.gaubert10@gmail.com>
2021-11-10 10:15:44 +0100

    GameController: move using position in session

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -7,6 +7,12 @@ use App\Model\LevelManager;
 class GameController extends AbstractController
 {
     private const VIEWPOINT_RADIUS = 2;
+    private const ACTION_OFFSETS = [
+        "up" => ['x' => 0, 'y' => -1],
+        "down" => ['x' => 0, 'y' => 1],
+        "left" => ['x' => -1, 'y' => 0],
+        "right" => ['x' => 1, 'y' => 0],
+    ];
 
     private function generateCellDetails(string $cellType, int $cellX, int $cellY, int $playerX, int $playerY): array
     {
@@ -80,19 +86,38 @@ class GameController extends AbstractController
         return $grid;
     }
 
+    private function getPosition(): array
+    {
+        return $_SESSION['position'] ?? ['x' => 0, 'y' => 0];
+    }
+
+    private function move(string $action): void
+    {
+        $position = $this->getPosition();
+
+        if (isset(self::ACTION_OFFSETS[$action])) {
+            $offsets = self::ACTION_OFFSETS[$action];
+            $position['x'] += $offsets['x'];
+            $position['y'] += $offsets['y'];
+        }
+
+        $_SESSION['position'] = $position;
+    }
 
     /**
      * Show first level
      */
-    public function index(?int $playerX, ?int $playerY): string
+    public function index(?string $action): string
     {
-        $playerX = $playerX ?? 0;
-        $playerY = $playerY ?? 0;
+        session_start();
+
+        $this->move($action ?? "");
+        $position = $this->getPosition();
 
         $levelManager = new LevelManager();
         $level = $levelManager->selectOneById(1);
         $cells = LevelManager::parseContent($level['content']);
-        $grid = $this->generateViewpoint($cells, $playerX, $playerY);
+        $grid = $this->generateViewpoint($cells, $position['x'], $position['y']);
 
         return $this->twig->render('Game/index.html.twig', ['level' => $level, 'grid' => $grid]);
     }
