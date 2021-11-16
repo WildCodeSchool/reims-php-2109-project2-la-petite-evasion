8f44c64c7030de50f596a5644bea3ccaea9af730
Sayn <marvincrepin@gmail.com>
2021-11-16 10:46:11 +0100

    RecordController: insertRecord method

--- src/Controller/RecordController.php
+++ src/Controller/RecordController.php
@@ -6,15 +6,30 @@ use App\Model\RecordManager;
 
 class RecordController extends AbstractController
 {
-/**
+    /**
      * Show first level
      */
+    private function insertRecord()
+    {
+        $errors = [];
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            if (empty($_POST['name']) || strlen($_POST['name']) > 30) {
+                $errors[] = "Ton nom ne peut pas être vide";
+            }
+
+            if (!$errors) {
+                $recordName = $_POST['name'];
+                $recordManager = new RecordManager();
+                $recordManager->insert($recordName);
+            }
+        }
+    }
     public function index(): string
     {
+        $this->insertRecord();
         $recordManager = new RecordManager();
         $records = $recordManager->selectAll('TIME');
 
-        return $this->twig->render('Record/leaderboard.html.twig', ['records' => $records]);
+        return $this->twig->render('Record/leaderboard.html.twig', ['records' => $records,]);
     }
-
 }
--- src/Model/RecordManager.php
+++ src/Model/RecordManager.php
@@ -5,4 +5,17 @@ namespace App\Model;
 class RecordManager extends AbstractManager
 {
     public const TABLE = 'record';
+
+    public function insert(array $record)
+    {
+        $statement = $this->pdo->prepare(
+            "INSERT INTO " . self::TABLE .
+            " (name, time, levelid) VALUES (:name, :time, :levelid)"
+        );
+        $statement->bindValue('name', $record['name'], \PDO::PARAM_STR);
+        $statement->bindValue('time', $record['time'], \PDO::PARAM_STR);
+
+        $statement->execute();
+        return (int)$this->pdo->lastInsertId();
+    }
 }
