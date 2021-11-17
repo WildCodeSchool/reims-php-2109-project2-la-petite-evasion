97338c69c8f49ba2768cf33ee02c81c508bd9a79
kbardy <k.bardy@laposte.net>
2021-11-17 17:18:16 +0100

    add form time

--- src/Controller/RecordController.php
+++ src/Controller/RecordController.php
@@ -25,7 +25,7 @@ class RecordController extends AbstractController
                 $record = [
                     'name' => $_POST['name'],
                     'time' => GameController::getFinishInterval(),
-                    'levelid' => GameController::getGameLevelId(),
+                    'level_id' => GameController::getGameLevelId(),
                 ];
 
                 $recordManager = new RecordManager();
@@ -33,6 +33,7 @@ class RecordController extends AbstractController
             }
         }
     }
+
     public function index(): string
     {
         $this->insertRecord();
