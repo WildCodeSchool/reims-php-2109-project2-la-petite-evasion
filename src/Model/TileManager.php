a7ce0d7dfc87ef2f040583cd01e0ebafe76a0729
Mario Rocha <mrocha51248@gmail.com>
2021-11-17 10:31:23 +0100

    TileManager: add selectByArea method

--- src/Model/TileManager.php
+++ src/Model/TileManager.php
@@ -69,6 +69,29 @@ class TileManager extends AbstractManager
         return $tiles;
     }
 
+    /*
+     * input: $area: array containing 'x' and 'y' position as well as 'width' and 'height' dimensions
+     */
+    public function selectByArea(array $area): array
+    {
+        $query =
+            "SELECT * FROM " . static::TABLE .
+            " WHERE level_id=:level_id AND x >= :min_x AND x < :max_x AND y >= :min_y AND y < :max_y" .
+            " ORDER BY y ASC, x ASC";
+        $statement = $this->pdo->prepare($query);
+        $statement->bindValue('level_id', $this->levelId, \PDO::PARAM_INT);
+        $statement->bindValue('min_x', $area['x'], \PDO::PARAM_INT);
+        $statement->bindValue('max_x', $area['x'] + $area['width'], \PDO::PARAM_INT);
+        $statement->bindValue('min_y', $area['y'], \PDO::PARAM_INT);
+        $statement->bindValue('max_y', $area['y'] + $area['height'], \PDO::PARAM_INT);
+        $statement->execute();
+        $tiles = [];
+        foreach ($statement->fetchAll() as $tile) {
+            $tiles[$tile['y']][$tile['x']] = $tile['type'];
+        }
+        return $tiles;
+    }
+
     /**
      * Get the value of levelId
      *
